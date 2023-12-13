1a56c5380ef51eab298d6537760ae9385fca40ed
williamvnobrega <williamvnobrega@gmail.com>
2023-12-13 15:26:15 -0300

    fix: refactor renderLeads function

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-let btnInput = document.querySelector("#input-btn");
-btnInput.addEventListener("click",()=>{console.log("I got clicked")})
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  renderLeads();
+});
+
+function renderLeads() {
+  let listItems = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    //listItems += "<li>" + `<a href=${myLeads[i]} target=_blank>`+ myLeads[i] + "</a>" +"</li>"
+    listItems += `
+        <li>
+            <a target='_blank' href=${myLeads[i]}>${myLeads[i]}</a>
+        </li>`;
+  }
+  ulEl.innerHTML = listItems;
+}
